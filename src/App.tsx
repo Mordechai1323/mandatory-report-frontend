a62f6adb81c0fe4a465c29ec2cbca4243e179787
Mordechai <mordechai@mail.com>
2024-04-10 16:02:43 +0300

    feat(app): update App component to use styled-components and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import {Routes, Router} from 'react-router-dom'
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { Layout } from './layout/Layout'
+import { PAGES } from './constants/pages'
+import { Themes } from './types/general'
+import { PageNotFound } from './pages/PageNotFound'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { darkTheme, lightTheme } from './styles/theme'
 
 function App() {
-  return <></>
+  const [theme, setTheme] = useState<Themes>('light')
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {PAGES.map((page) => (
+            <Route key={page.path} path={page.path} element={<page.component />} />
+          ))}
+
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
+  )
 }
 
 export default App
