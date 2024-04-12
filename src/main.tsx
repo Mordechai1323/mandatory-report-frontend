d61d6ef8012cc2855a284654328112da4727b245
Mordechai <mordechai@mail.com>
2024-04-12 16:45:50 +0300

    feat(provider): add QueryClientProvider for improved data fetching with react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
 import { EventProvider } from './context/EventProvider.tsx'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <EventProvider>
-        <App />
-      </EventProvider>
+      <QueryClientProvider client={queryClient}>
+        <EventProvider>
+          <App />
+        </EventProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
