23f5df054dae38377aead6bab15c4d75016e3f7b
Mordechai <mordechai@mail.com>
2024-04-12 17:02:07 +0300

    feat(ui): added home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import styled from 'styled-components'
+
+import { AddReport } from './AddReport'
+import { Event } from '../../components/Events/Event'
+import { ReportsTable } from '../../components/Reports/ReportsTable'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <HomeStyle>
+      <Event />
+      <ReportsTable />
+      <AddReport />
+    </HomeStyle>
   )
 }
 
+const HomeStyle = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+
+  margin-top: 3rem;
+  .reports-table {
+    width: 90%;
+    height: 90%;
+    overflow: auto;
+  }
+`
