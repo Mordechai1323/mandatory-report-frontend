a86918de77eaddcda12c286ad9140c96a2af62f0
Mordechai <mordechai@mail.com>
2024-04-13 17:45:41 +0300

    fix(ui): remove React.StrictMode due to socket.io bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -13,7 +12,6 @@ import { ReportsTypesProvider } from './context/ReportsTypesProvider.tsx'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <EventProvider>
@@ -29,5 +27,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </EventProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </React.StrictMode>
 )
