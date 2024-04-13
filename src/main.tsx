dffed86316c26f7ff652f9fec306c5393739b0c4
Mordechai <mordechai@mail.com>
2024-04-13 12:53:50 +0300

    feat(context): add providers for global saving of areas, all events, departments, and report types

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
 import { EventProvider } from './context/EventProvider.tsx'
+import { AreasProvider } from './context/AreasProvider.tsx'
+import { AllEventsProvider } from './context/AllEventsProvider.tsx'
+import { DepartmentsProvider } from './context/DepartmentsProvider.tsx'
+import { ReportsTypesProvider } from './context/ReportsTypesProvider.tsx'
 
 const queryClient = new QueryClient()
 
@@ -13,7 +17,15 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <EventProvider>
-          <App />
+          <AllEventsProvider>
+            <DepartmentsProvider>
+              <AreasProvider>
+                <ReportsTypesProvider>
+                  <App />
+                </ReportsTypesProvider>
+              </AreasProvider>
+            </DepartmentsProvider>
+          </AllEventsProvider>
         </EventProvider>
       </QueryClientProvider>
     </BrowserRouter>
