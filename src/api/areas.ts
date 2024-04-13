0884351f1075905f5a550f247bff2707ea09040c
Mordechai <mordechai@mail.com>
2024-04-13 10:24:39 +0300

    feat(api): add API functions for fetching, creating, updating, and deleting areas

---
+++ src/api/areas.ts
@@ -0,0 +1,45 @@
+import { axiosPrivate } from './api'
+import { Area } from '../models/area'
+import { handlerError } from '../utils/handlerError'
+
+const fetchAreas = async () => {
+  try {
+    const response = await axiosPrivate<Area[]>('/areas')
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const createArea = async (area: Area) => {
+  try {
+    const response = await axiosPrivate.post<Area>('/areas', area)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const updateArea = async (area: Area) => {
+  try {
+    const response = await axiosPrivate.put<Area>(`/areas/${area.id}`, area)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const deleteArea = async (id: number) => {
+  try {
+    const response = await axiosPrivate.delete<Area>(`/areas/${id}`)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+export { fetchAreas, createArea, updateArea, deleteArea }
