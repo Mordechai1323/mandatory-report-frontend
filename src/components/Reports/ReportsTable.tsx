34f42243c0d66329be3ba6e8a59513d8bace1f57
Mordechai <mordechai@mail.com>
2024-04-13 18:30:24 +0300

    feat(ui): add AddReportFormPopup for editing reports and refactor ReportsTable component

--- src/components/Reports/ReportsTable.tsx
+++ src/components/Reports/ReportsTable.tsx
@@ -1,51 +1,50 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Socket, io } from 'socket.io-client'
 
-import { BASE_URL } from '../../api/api'
 import { ReportItem } from './ReportItem'
 import { Report } from '../../models/report'
-import { useEvent } from '../../hooks/useEvent'
 import { ReportsHeader } from './ReportsHeader'
+import { ReportFormPopup } from '../../pages/Home/ReportFormPopup'
+import { useReports } from '../../hooks/useReports'
 
 export const ReportsTable = () => {
-  const [reports, setReports] = React.useState<Report[]>()
-  const { event } = useEvent()
-  const socket: Socket = io(BASE_URL)
+  const [reportToEdit, setReportToEdit] = React.useState<Report>()
+  const [isEditReportPopupOpen, setIsEditReportPopupOpen] = React.useState(false)
 
-  React.useEffect(() => {
-    if (event) {
-      socket.on('connect', () => console.log(socket.id))
-      socket.emit('joinRoom', event.id)
-      socket.on('joinedRoomSuccessfully', (res) => {
-        if (res === `Joined to room ${event.id} successfully`) {
-          socket.emit('getReports', { eventId: event.id, page: 1 })
-          socket.on('reports', (reports: Report[]) => {
-            console.log(reports)
+  const { reports } = useReports()
 
-            setReports(reports)
-          })
-        }
-      })
-    }
+  const editReportHandler = (report: Report) => {
+    setReportToEdit(report)
+    setIsEditReportPopupOpen(true)
+  }
 
-    return () => {
-      socket.disconnect()
-    }
-  }, [event])
+  const handelCloseEditReportPopup = () => {
+    setIsEditReportPopupOpen(false)
+    setReportToEdit(undefined)
+  }
 
   return (
     <ReportsTableStyle>
       <ReportsHeader />
       {reports?.map((report) => {
-        return <ReportItem report={report} key={report.id} />
+        return <ReportItem report={report} key={report.id} editReportHandler={editReportHandler} />
       })}
+      {reportToEdit && (
+        <ReportFormPopup
+          isOpen={isEditReportPopupOpen}
+          handleClose={handelCloseEditReportPopup}
+          title="עריכת דיווח"
+          report={reportToEdit}
+        />
+      )}
     </ReportsTableStyle>
   )
 }
 
 const ReportsTableStyle = styled.div`
-  width: 90%;
+  width: 92%;
+  padding-left: 0.5%;
   height: 90%;
   overflow: auto;
+  margin-top: 2rem;
 `
