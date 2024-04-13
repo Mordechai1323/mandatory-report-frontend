1f412a138daaec885ca2253615e68be3756d283d
Mordechai <mordechai@mail.com>
2024-04-13 10:24:54 +0300

    feat(api): add API functions for fetching, creating, updating, and deleting departments

---
+++ src/api/departments.ts
@@ -0,0 +1,45 @@
+import { Department } from '../models/department'
+import { handlerError } from '../utils/handlerError'
+import { axiosPrivate } from './api'
+
+const fetchDepartments = async () => {
+  try {
+    const response = await axiosPrivate<Department[]>('/departments')
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const createDepartment = async (department: Department) => {
+  try {
+    const response = await axiosPrivate.post<Department>('/departments', department)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const updateDepartment = async (department: Department) => {
+  try {
+    const response = await axiosPrivate.put<Department>(`/departments/${department.id}`, department)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+const deleteDepartment = async (id: number) => {
+  try {
+    const response = await axiosPrivate.delete<Department>(`/departments/${id}`)
+
+    return response.data
+  } catch (error) {
+    handlerError(error)
+  }
+}
+
+export { fetchDepartments, createDepartment, updateDepartment, deleteDepartment }
