573d7e94a25227369c8b2c167e25ad16f3166761
Mordechai <mordechai@mail.com>
2024-04-13 09:33:21 +0300

    feat(ui): add AddReport component to open add report popup on click

--- src/pages/Home/AddReport.tsx
+++ src/pages/Home/AddReport.tsx
@@ -2,9 +2,13 @@ import styled from 'styled-components'
 
 import addReportIcon from '../../assets/icons/plus.svg'
 
-export const AddReport = () => {
+interface AddReportProps {
+  openAddReportPopup: () => void
+}
+
+export const AddReport = ({ openAddReportPopup }: AddReportProps) => {
   return (
-    <AddReportStyled>
+    <AddReportStyled onClick={openAddReportPopup}>
       <img src={addReportIcon} alt="add report" />
     </AddReportStyled>
   )
@@ -12,10 +16,11 @@ export const AddReport = () => {
 
 const AddReportStyled = styled.div`
   position: absolute;
-  right: 2rem;
-  bottom: 3rem;
+  right: 0.9rem;
+  bottom: 1.9rem;
   height: 4rem;
   width: 4rem;
+  cursor: pointer;
 
   & img {
     width: 100%;
