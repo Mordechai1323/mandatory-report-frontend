3405489e4ab0657b198a4ff458b68136a0ae6e5f
Mordechai <mordechai@mail.com>
2024-04-13 13:11:22 +0300

    feat(schema): add report schema and report form type

--- src/models/report.ts
+++ src/models/report.ts
@@ -1,7 +1,8 @@
+import { z } from 'zod'
 import { Area } from './area'
 import { Department } from './department'
 import { Event } from './event'
-import { ReportsType } from './reportType'
+import { ReportType } from './reportType'
 
 export type Report = {
   id: number
@@ -11,7 +12,22 @@ export type Report = {
   area: Area
   department: Department
   event: Event
-  reportType: ReportsType
+  reportType: ReportType
   createdAt: string
   updatedAt: string
 }
+
+export const reportSchema = z.object({
+  id: z.number().positive().optional(),
+  content: z
+    .string()
+    .trim()
+    .min(2, 'דיווח חייב להיות במינימום 2 תווים')
+    .max(255, 'דיווח חייב להיות  במקסימום 255 תווים'),
+  isImportant: z.boolean().default(false),
+  areaId: z.number({ required_error: 'בחר זירה' }).positive('בחר זירה חוקית'),
+  departmentId: z.number({ required_error: 'בחר מדור' }).positive('בחר מדור חוקי'),
+  reportTypeId: z.number({ required_error: 'בחר מהוות הדיווח' }).positive('בחר מהוות הדיווח חוקי'),
+})
+
+export type ReportForm = z.infer<typeof reportSchema>
