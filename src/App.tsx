14c94b6bb6283fd45ab4c5f499f9786279f42165
Mordechai1323 <mordechaib23@gmail.com>
2024-04-29 12:57:16 +0300

    feat(ui): add import of ToastContainer and ReactToastify CSS in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { ToastContainer } from 'react-toastify'
 
 import { Layout } from './layout/Layout'
 import { PAGES } from './constants/pages'
@@ -8,12 +9,15 @@ import { PageNotFound } from './pages/PageNotFound'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { emergencyTheme, trainingTheme } from './styles/theme'
 
+import 'react-toastify/dist/ReactToastify.css'
+
 function App() {
   const { event } = useEvent()
 
   return (
     <ThemeProvider theme={event?.isTraining ? trainingTheme : emergencyTheme}>
       <GlobalStyles />
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Layout />}>
           {PAGES.map((page) => (
