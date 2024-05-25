db9ae16ffac138a9cad6ea0cbfa3b1e8c7a5bc56
Mordechai1323 <mordechaib23@gmail.com>
2024-05-25 20:51:29 +0300

    feat: Update Filters component to include additional filters and options

--- src/pages/Home/FiltersAndTabs/Filters/Filters.tsx
+++ src/pages/Home/FiltersAndTabs/Filters/Filters.tsx
@@ -1,14 +1,53 @@
 import styled from 'styled-components'
 
 import { Filter } from './Filter'
-import { filtersOptions } from '../../../../constants/filtersOptions'
+import { useAreas } from '../../../../hooks/useAreas'
+import { useDepartments } from '../../../../hooks/useDepartments'
+import { useReportsTypes } from '../../../../hooks/useReportsTypes'
+import { filterByTime, sortBy } from '../../../../constants/filtersOptions'
 
 export const Filters = () => {
+  const { areas } = useAreas()
+  const { departments } = useDepartments()
+  const { reportsTypes } = useReportsTypes()
+
   return (
     <FiltersStyle>
-      {filtersOptions.map((filter) => (
-        <Filter key={filter.value} filter={filter} />
-      ))}
+      <Filter filter={filterByTime} />
+      <Filter filter={sortBy} />
+      <Filter
+        filter={{
+          value: 'area',
+          label: 'זירה',
+          filterType: 'checkbox',
+          options: areas?.map((area) => ({
+            label: area.name,
+            value: area.id,
+          })),
+        }}
+      />
+      <Filter
+        filter={{
+          value: 'department',
+          label: 'מכלול מדווח',
+          filterType: 'checkbox',
+          options: departments?.map((department) => ({
+            label: department.name,
+            value: department.id,
+          })),
+        }}
+      />
+      <Filter
+        filter={{
+          value: 'reportType',
+          label: 'מהות הדיווח',
+          filterType: 'checkbox',
+          options: reportsTypes?.map((reportType) => ({
+            label: reportType.name,
+            value: reportType.id,
+          })),
+        }}
+      />
     </FiltersStyle>
   )
 }
