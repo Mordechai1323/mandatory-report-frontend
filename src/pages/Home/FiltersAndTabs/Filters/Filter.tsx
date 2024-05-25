4da5215a2fe36478bee7a8df84959cf5ae89a41c
Mordechai1323 <mordechaib23@gmail.com>
2024-05-25 15:10:21 +0300

    feat: add Filter component for filter popup options

---
+++ src/pages/Home/FiltersAndTabs/Filters/Filter.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { FilterPopup } from './FilterPopup'
+import openFilter from '../../../../assets/icons/openFilter.svg'
+import { filterOption } from '../../../../constants/filtersOptions'
+import { useClickOutSide } from '../../../../hooks/useClickOutSide'
+
+interface FilterProps {
+  filter: filterOption
+}
+
+export const Filter = ({ filter }: FilterProps) => {
+  const [isFilterPopupOpen, setIsFilterPopupOpen] = React.useState(false)
+
+  const { ref } = useClickOutSide<HTMLDivElement>(() => setIsFilterPopupOpen(false))
+
+  return (
+    <FilterStyle ref={ref}>
+      <FilterDetails onClick={() => setIsFilterPopupOpen((prev) => !prev)}>
+        <span>{filter.label}</span>
+        <img src={openFilter} alt="open filter icon" />
+      </FilterDetails>
+      {isFilterPopupOpen && <FilterPopup filter={filter}  />}
+    </FilterStyle>
+  )
+}
+
+const FilterStyle = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+
+const FilterDetails = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
+
+  & img {
+    margin-right: 1rem;
+  }
+
+  & span {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`
