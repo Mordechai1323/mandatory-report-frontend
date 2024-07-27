8511a45807308cdae240fcfee7c6fc63e2ec2481
Mordechai1323 <mordechaib23@gmail.com>
2024-07-27 17:21:49 +0300

    fix: change hight to be 100 of screen

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -23,7 +23,7 @@ export const PageNotFound = () => {
 
 const PageNotFoundStyle = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
